eff51cbe6372b669318d642ca5d9788439feacd2
Ron-ron Aspe Rivera <ronaspe42@gmail.com>
2026-01-07 20:15:02 +0800

    added a responsive design for on the header or navbar, changes in resume

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,23 +1,43 @@
-import React from "react"
+import { useEffect, useState } from "react";
 import DarkModeToggler from "./DarkModeToggler.jsx";
 import {Link} from "react-router";
 
 function Header() {
-  return (
-    <>
-      <div className="flex justify-center">
-        <div className="flex items-center justify-between w-full max-w-2xl my-5">
-          <Link to="/" className="text-xl font-bold mr-14">RON</Link> 
-          <div className="flex items-center text-sm gap-3">
-            <Link to="/projects">projects</Link>
-            <Link to="/about-me">about me</Link>
-            <a href="https://drive.google.com/file/d/183L2NUmVwGZX3guNMNyy5tiY7NsRjjS9/view?usp=sharing" target="_blank">resume</a>
-           <DarkModeToggler/>
-          </div>
-        </div>
-      </div>
-  </>
-  );
+
+    const minWidht = 450;
+     
+    const [isSmall, setIsSmall] = useState(false);
+
+    useEffect(() =>{
+
+        function handleResize(){
+            setIsSmall(window.innerWidth <= minWidht);
+        }
+
+        window.addEventListener("resize", handleResize);
+        
+        return () => window.removeEventListener("resize", handleResize)
+
+    }, [minWidht])
+
+    return (
+        <>
+            <div className="flex justify-center">
+                <div className="flex items-center justify-between w-full max-w-2xl my-5">
+                    <Link to="/" className="text-xl font-bold mr-14">RON</Link> 
+                    <div className="flex items-center text-sm gap-3">
+                        <Link to="/projects">projects</Link>
+                        <Link to="/about-me">{isSmall? 
+                            (<span>me</span>)
+                            :
+                            (<span>about me</span>)}</Link>
+                        <a href="https://drive.google.com/file/d/1fI-WcuNkkCeahmX9o55pWb_k620zcvsT/view?usp=sharing" target="_blank">resume</a>
+                        <DarkModeToggler/>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Header;
