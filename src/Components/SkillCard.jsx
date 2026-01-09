8a4eb7fc0c69298801f53033d5de852508ba9546
Ron-ron Aspe Rivera <ronaspe42@gmail.com>
2026-01-09 14:39:34 +0800

    fix icon colors in light mode

--- src/Components/SkillCard.jsx
+++ src/Components/SkillCard.jsx
@@ -6,7 +6,7 @@ function SkillCard({ Icon, label, color, subLabel }) {
       className="group flex flex-col items-center justify-center w-24 h-24 p-2 border border-white rounded-lg dark:border-gray-900 transition duration-300 ease-in-out hover:shadow-xl hover:scale-110"
       style={wrapperStyle}
     >
-      <Icon className="w-full h-auto text-gray-50 transition-colors duration-300 group-hover:text-[var(--icon-hover-color)]" />
+      <Icon className="w-full h-auto text-white dark:text-gray-900 transition-colors duration-300 group-hover:text-[var(--icon-hover-color)]" />
 
       <span className="mt-3 text-sm font-medium">{label}</span>
       {subLabel && <span className="mt-1 text-sm font-medium">{subLabel}</span>}
