7b8c3098319a48fbf371062a81f15108badad0a3
Ron-ron Aspe Rivera <ronaspe42@gmail.com>
2026-02-21 01:01:09 +0800

    old backend domain

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development"
     ? "http://localhost:8080/api" // local dev backend
-    : "api.ronronrivera.tech",// production (Vercel)
+    : "my-portfolio-backend-five-zeta.vercel.app/api",// production (Vercel)
   headers: {
     "Content-Type": "application/json", // ensure JSON is always sent
   },
